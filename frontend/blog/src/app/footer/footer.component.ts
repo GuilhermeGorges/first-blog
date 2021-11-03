b14f8465c7a7519b1f64f256b39fe3c32957c8a6
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-11-03 19:54:15 -0300

    Implement contact, footer, home, navbar html

--- frontend/blog/src/app/footer/footer.component.ts
+++ frontend/blog/src/app/footer/footer.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { faInstagram, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+    faInstagram = faInstagram
+    faFacebook = faFacebook
+    faLinkedin = faLinkedin
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
